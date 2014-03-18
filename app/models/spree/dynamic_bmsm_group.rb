aa78b5d9ac690f66e900d0878cb134f4f0364ebb
Uri Mikhli <urimikhli@gmail.com>
2014-03-18 16:49:25 -0400

    add attr_accessible to the models

--- app/models/spree/dynamic_bmsm_group.rb
+++ app/models/spree/dynamic_bmsm_group.rb
@@ -1,3 +1,3 @@
 class Spree::DynamicBmsmGroup < ActiveRecord::Base
-  has_many :spree_dynamic_bmsm_tiers
+  attr_accessible :name, :description
 end
--- app/models/spree/dynamic_bmsm_tier.rb
+++ app/models/spree/dynamic_bmsm_tier.rb
@@ -1,3 +1,4 @@
 class Spree::DynamicBmsmTier < ActiveRecord::Base
+  attr_accessible :label, :level, :discount
   belongs_to :spree_dynamic_bmsm_groups
 end
