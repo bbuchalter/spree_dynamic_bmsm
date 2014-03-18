127413abff984d94bab51ec6346eee5aab6a578e
Uri Mikhli <urimikhli@gmail.com>
2014-03-18 16:50:46 -0400

    controller should be accessible from out of admin as well

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Spree::Core::Engine.routes.draw do
 
   # Add your extension routes here
 
+  resources :dynamic_bmsm_groups
+
+  resources :dynamic_bmsm_tiers
   namespace :admin do
     resources :dynamic_bmsm_groups
 
