c4d13078f024ef3733ee9441aa658a831247c7b8
Uri Mikhli <urimikhli@gmail.com>
2014-03-18 12:22:33 -0400

    change reference to integer

--- db/migrate/20140317162110_create_spree_dynamic_bmsm_tiers.rb
+++ db/migrate/20140317162110_create_spree_dynamic_bmsm_tiers.rb
@@ -1,7 +1,7 @@
 class CreateSpreeDynamicBmsmTiers < ActiveRecord::Migration
   def change
     create_table :spree_dynamic_bmsm_tiers do |t|
-      t.reference :spree_dynamic_bmsm_groups
+      t.integer :spree_dynamic_bmsm_groups_id
       t.string :label
       t.float :level
       t.float :discount
