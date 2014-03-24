2edb053651334bd321156e86d15c65e888981869
Uri Mikhli <urimikhli@gmail.com>
2014-03-24 17:00:26 -0400

    Add :original to tab override

--- app/overrides/dynamic_bmsm_admin_tab.rb
+++ app/overrides/dynamic_bmsm_admin_tab.rb
@@ -2,5 +2,6 @@ Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "dynamic_bmsm_admin_tab",
                      :insert_bottom => "[data-hook='admin_tabs']",
                      :text => "<%= tab(:bmsm, :url => admin_dynamic_bmsm_tiers_path) %>",
-                     :disabled => false
+                     :disabled => false,
+                     :original => 'd3a609b80fae6e8d0ce986617f17009db48bd66c'
                     )
