a6bacb09a73dc7b2dbe617094f076cf92cee6482
Uri Mikhli <urimikhli@gmail.com>
2014-03-28 13:56:28 -0400

    Working Set bmsm group method in user decorator

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -12,23 +12,31 @@ Spree::User.class_eval do
   end
 
   def set_bmsm_group(default=0)
-    group_list = Spree::Spree::DynamicBmsmGroup.all.map{|x|x.id}
+    ##always set the group. Its random unless assigned.
+    #major assumption is that group id 1 exists
+    group_list = Spree::DynamicBmsmGroup.all.map{|x|x.id}
+    tiers = []
 
     ## if a default is given set it as the group
     ## unless it doesnt exist, at which point,
     ## just assign a random 'Existing' group
-    if default != 0 && group_list.grep(default).count == 1
+    if default != 0 && group_list.present? && group_list.grep(default).count == 1
       ##assign group ID
       group = default
+      tiers =  Spree::DynamicBmsmTier.where(:dynamic_bmsm_group_id => group)
     else
       ## random assignment
       index_range = group_list.count
       group = group_list[Random.rand(index_range)]   #arrays are 0 indexed as is rand first value
+      tiers =  Spree::DynamicBmsmTier.where(:dynamic_bmsm_group_id => group)
+    end
+    if tiers.count == 0
+      self.set_bmsm_group #beware recursion, here be dragons!... keep random walking until you hit on a group that has tiers.
+    else
+      self.update_column(:spree_dynamic_bmsm_group_id,group)
+
+      return group #return the group that was set
     end
 
-    ##always set the group. Its random unless assigned.    
-    self.update_column(:spree_dynamic_bmsm_group_id,group)
-
-    return group #return the group that was set
   end
 end
